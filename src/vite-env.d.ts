21309930664ea81bfc2eb406e467a6a7b2246373
ramniel <ramniel.labaut@gmail.com>
2022-04-28 17:44:23 -0300

    feat: provide type definition for env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+// eslint-disable-next-line no-unused-vars
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_APP_DESCRIPTION: string;
+  // more env variables...
+}
